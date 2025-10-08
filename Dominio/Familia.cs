f6d01fdbbc011710b775bbd5f8b1fdfafc0007b6
Lucas Sirineu <sirineu33@gmail.com>
2025-10-07 23:41:31 -0300

    Criando classe Familia

---
+++ Dominio/Familia.cs
@@ -0,0 +1,17 @@
+public class Familia
+{
+    public Guid Id { get; private set; }
+    public Pretendente Titular { get; private set; }
+    public Pretendente? Conjuge { get; private set; }
+    private List<Dependente> _dependentes = new List<Dependente>();
+    public IEnumerable<Dependente> Dependentes => _dependentes;
+
+    public Familia(Pretendente titular, IEnumerable<Dependente> dependentes, Pretendente? conjuge = null)
+    {
+        Id = Guid.NewGuid();
+        Titular = titular;
+        Conjuge = conjuge;
+        _dependentes.AddRange(dependentes);
+    }
+
+}
